c3ec37e6c6a072eafc89edeb0a171962116e0cbd
levs2001 <50678585+levs2001@users.noreply.github.com>
2020-10-04 08:26:49 +0300

    Путь к файлу теперь из аргументы командной строки, либо через ввод в консоль.

--- Lab_1_3sem/Source.c
+++ Lab_1_3sem/Source.c
@@ -6,47 +6,67 @@
 #include <stdlib.h>
 
 
-void PrintList(myList *man)
+void PrintList(myList* man)
 {
-  while (man != NULL)
-  {
-    printf("%s %s %s\n", man->surname, man->name, man->patron);
-    man = man->next;
-  }
+	while (man != NULL)
+	{
+		printf("%s %s %s\n", man->surname, man->name, man->patron);
+		man = man->next;
+	}
 }
 
-int main(void) {
-  myList* start = NULL;
-  myList* nMan = NULL;
-  FILE* file = NULL;
-  SNP snp;
-  char buf[20];
-  char* word;
-  file = fopen("D:\\Programms\\ForLab1_3sem\\List.txt", "r");
-  
-  if (file == NULL) {
-      printf("The file manList wasn't open");
-      return 0;
-  }
+int main(int argc, char* argv[]) {
+	myList* start = NULL;
+	myList* nMan = NULL;
+	FILE* file = NULL;
+	SNP snp;
+	char buf[20], filename[50];
+	char* word;
 
-  //ВВодим первого в список
-  fscanf(file, "%s", snp.surname);
-  fscanf(file, "%s", snp.name);
-  fscanf(file, "%s", snp.patron);
-  
-  start = InitList(snp.surname, snp.name, snp.patron);
-  ////
-  
-  
-  while (!feof(file)) {
-    fscanf(file, "%s", snp.surname);
-    fscanf(file, "%s", snp.name);
-    fscanf(file, "%s", snp.patron);
-    nMan = Add2List(&start, snp);
-  }
-  
-  PrintList(start);
-  DeleteList(start);
-  fclose(file);
-  return 0;
+	switch (argc) {
+	case 1: {
+		printf("Enter filename.\n");
+		scanf("%s", filename);
+		//Быстрый ввод для удобства
+		if (filename[0] == '1')
+			strcpy(filename, "D:\\Programms\\ForLab1_3sem\\List.txt");
+		//
+		break;
+	}
+	case 2: {
+		strcpy(filename, argv[1]);
+		break;
+	}
+	default: {
+		printf("Not correct list of files!");
+		return 1;
+	}
+	}
+	file = fopen(filename, "r");
+
+	if (file == NULL) {
+		printf("The file manList wasn't open");
+		return 2;
+	}
+
+	//ВВодим первого в список
+	fscanf(file, "%s", snp.surname);
+	fscanf(file, "%s", snp.name);
+	fscanf(file, "%s", snp.patron);
+
+	start = InitList(snp.surname, snp.name, snp.patron);
+	////
+
+
+	while (!feof(file)) {
+		fscanf(file, "%s", snp.surname);
+		fscanf(file, "%s", snp.name);
+		fscanf(file, "%s", snp.patron);
+		nMan = Add2List(&start, snp);
+	}
+
+	PrintList(start);
+	DeleteList(start);
+	fclose(file);
+	return 0;
 }
